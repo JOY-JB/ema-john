a43ffc7056779b17d752010180e6b37e83a45e97
joy-jb <joybarua0058@gmail.com>
2021-10-26 21:49:00 +0600

    firebase authentication test

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvfEh6DnTx2KQXbhuZT95witO5mBYi-Xw",
+    authDomain: "ema-john-authentication-214a0.firebaseapp.com",
+    projectId: "ema-john-authentication-214a0",
+    storageBucket: "ema-john-authentication-214a0.appspot.com",
+    messagingSenderId: "671191174179",
+    appId: "1:671191174179:web:ff7d94a5c0876b13a4f596",
+    measurementId: "G-8XQ8FDNTWR"
+};
+
+export default firebaseConfig;
